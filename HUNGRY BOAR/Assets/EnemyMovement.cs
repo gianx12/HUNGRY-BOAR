70ef329ae6f5a415dc7091d0659fd39baa2c4e31
Francesco De Luca <francy.delc01@gmail.com>
2023-04-14 19:50:42 +0200

    Ulteriori cambiamenti al livello

--- HUNGRY BOAR/Assets/EnemyMovement.cs
+++ HUNGRY BOAR/Assets/EnemyMovement.cs
@@ -9,57 +9,30 @@ public class EnemyMovement : MonoBehaviour
     public float moveSpeed;
     public int patrolDestination;
 
-    public Transform playerTransform;
-    public bool isChasing;
-    public float chaseDistance;
-
     // Update is called once per frame
     void Update()
     {
-        if (isChasing)
+
+        if (patrolDestination == 0)
         {
-            if(transform.position.x > playerTransform.position.x)
+            transform.position = Vector2.MoveTowards(transform.position, patrolPoints[0].position, moveSpeed * Time.deltaTime);
+            if (Vector2.Distance(transform.position, patrolPoints[0].position) < .2f)
             {
-                transform.position += Vector3.left * moveSpeed * Time.deltaTime;
-            }
-
-            if (transform.position.x < playerTransform.position.x)
-            {
-                transform.position += Vector3.right * moveSpeed * Time.deltaTime;
-            }
-
-        } else
-        {
-            if(Vector2.Distance(transform.position, playerTransform.position) < chaseDistance)
-            {
-                isChasing = true;
-            }
-
-            if (patrolDestination == 0)
-            {
-                transform.position = Vector2.MoveTowards(transform.position, patrolPoints[0].position, moveSpeed * Time.deltaTime);
-                if (Vector2.Distance(transform.position, patrolPoints[0].position) < .2f)
-                {
-                    transform.localScale = new Vector3(-1, 1, 1);
-                    patrolDestination = 1;
-                }
-            }
-
-            if (patrolDestination == 1)
-            {
-                transform.position = Vector2.MoveTowards(transform.position, patrolPoints[1].position, moveSpeed * Time.deltaTime);
-                if (Vector2.Distance(transform.position, patrolPoints[1].position) < .2f)
-                {
-                    transform.localScale = new Vector3(1, 1, 1);
-                    patrolDestination = 0;
-                }
+                transform.localScale = new Vector3(-1, 1, 1);
+                patrolDestination = 1;
             }
         }
-    } 
 
-
-
-
+        if (patrolDestination == 1)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, patrolPoints[1].position, moveSpeed * Time.deltaTime);
+            if (Vector2.Distance(transform.position, patrolPoints[1].position) < .2f)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+                patrolDestination = 0;
+            }
+        }
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -69,3 +42,4 @@ public class EnemyMovement : MonoBehaviour
         }
     }
 }
+       
